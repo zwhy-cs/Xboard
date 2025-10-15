47244f09cf54b512e992086d763be762473392df
zwhy <125182767+zwhy-cs@users.noreply.github.com>
2025-10-15 19:04:41 +0800

    Update ClashMeta.php

--- app/Protocols/ClashMeta.php
+++ app/Protocols/ClashMeta.php
@@ -329,7 +329,7 @@ class ClashMeta extends AbstractProtocol
                     'public-key' => data_get($protocol_settings, 'reality_settings.public_key'),
                     'short-id' => data_get($protocol_settings, 'reality_settings.short_id')
                 ];
-                $array['client-fingerprint'] = 'firefox';
+                $array['client-fingerprint'] = 'chrome';
                 $array['network'] = 'tcp';
                 break;
             default:
